b30b725f18ca5373226f12613c1ec7ec661a7449
tyrotrader <tyrotrader@ef3fde84-0b2f-0410-9c2a-95ac4a8bd949>
2007-04-26 22:52:48 +0000

    Initial import.

---
+++ trunk/com.ats.client.runtime/src/com/ats/client/runtime/Perspective.java
@@ -0,0 +1,30 @@
+package com.ats.client.runtime;
+
+import org.eclipse.ui.IFolderLayout;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+import com.ats.client.runtime.views.MessagesView;
+import com.ats.client.runtime.views.StrategyStatusView;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+		String editorArea = layout.getEditorArea();
+		layout.setEditorAreaVisible(false);
+		
+		IFolderLayout folder = layout.createFolder("messageView", IPageLayout.BOTTOM, 0.65f, editorArea);
+		folder.addView(MessagesView.ID);
+		layout.getViewLayout(MessagesView.ID).setCloseable(false);
+		
+		
+		folder = layout.createFolder("messages", IPageLayout.TOP, 0.75f, editorArea);
+		folder.addPlaceholder(StrategyStatusView.ID + ":*");
+		//folder.addView(StrategyStatusView.ID);
+		
+
+		//layout.getViewLayout(MessageView.ID).setCloseable(false);
+		
+
+	}
+}
