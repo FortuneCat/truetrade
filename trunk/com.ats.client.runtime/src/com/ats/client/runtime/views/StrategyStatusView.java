7fde3135d87a5927377a8c4058047adbf0466993
tyrotrader <tyrotrader@ef3fde84-0b2f-0410-9c2a-95ac4a8bd949>
2007-04-27 23:30:05 +0000

    Table View instead of Table PositionManager listener added

--- trunk/com.ats.client.runtime/src/com/ats/client/runtime/views/StrategyStatusView.java
+++ trunk/com.ats.client.runtime/src/com/ats/client/runtime/views/StrategyStatusView.java
@@ -1,23 +1,47 @@
 package com.ats.client.runtime.views;
 
+import static com.ats.engine.PositionManager.PROP_ADD_POSITION;
+import static com.ats.engine.PositionManager.PROP_EXECUTION;
+import static com.ats.engine.PositionManager.PROP_RESET;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.ui.part.ViewPart;
 
+import com.ats.engine.JExecution;
+import com.ats.engine.PositionManager;
 import com.ats.engine.StrategyDefinition;
+import com.ats.platform.Position;
+import com.ats.utils.Utils;
 
 public class StrategyStatusView extends ViewPart {
 
 	public static final String ID = "com.ats.client.runtime.views.strategyStatusView";
 	
-	private Table positionTable;
-	private Table executionTable;
+	private TableViewer positionTable;
+	private TableViewer executionTable;
+	
+	private List<Position> positions;
+	private List<JExecution> executions;
 	
 	private StrategyDefinition strategyDefinition;
 	
@@ -25,6 +49,7 @@ public class StrategyStatusView extends ViewPart {
 		this.strategyDefinition = stratDef;
 	    this.setPartName(strategyDefinition.getStrategyClass().getSimpleName());
 	}
+	
 
 	@Override
 	public void createPartControl(Composite parent) {
@@ -39,33 +64,36 @@ public class StrategyStatusView extends ViewPart {
 	    posComp.setLayout(new GridLayout());
 	    posItem.setControl(posComp);
 	    
-	    positionTable = new Table(posComp, SWT.MULTI | SWT.FULL_SELECTION);
-	    positionTable.setHeaderVisible(true);
-	    positionTable.setLinesVisible(true);
-	    positionTable.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+	    positionTable = new TableViewer(posComp, SWT.MULTI | SWT.FULL_SELECTION);
+	    positionTable.getTable().setHeaderVisible(true);
+	    positionTable.getTable().setLinesVisible(true);
+	    positionTable.getTable().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+	    positionTable.setContentProvider(new PositionContentProvider());
+	    positionTable.setLabelProvider(new PositionLabelProvider());
+	    //positionTable.setInput()
 	    
-	    
-	    TableColumn column = new TableColumn(positionTable, SWT.NONE);
+	    Table table = positionTable.getTable();
+	    TableColumn column = new TableColumn(table, SWT.NONE);
 	    column.setText("Symb");
 	    column.setWidth(60);
 	    
-	    column = new TableColumn(positionTable, SWT.NONE);
+	    column = new TableColumn(table, SWT.NONE);
 	    column.setText("Dir");
 	    column.setWidth(60);
 	    
-	    column = new TableColumn(positionTable, SWT.NONE);
+	    column = new TableColumn(table, SWT.NONE);
 	    column.setText("Size");
 	    column.setWidth(60);
 	
-	    column = new TableColumn(positionTable, SWT.NONE);
+	    column = new TableColumn(table, SWT.NONE);
 	    column.setText("Avg Cost");
 	    column.setWidth(60);
 	    
-	    column = new TableColumn(positionTable, SWT.NONE);
+	    column = new TableColumn(table, SWT.NONE);
 	    column.setText("UnrP&L");
 	    column.setWidth(60);
 	    
-	    column = new TableColumn(positionTable, SWT.NONE);
+	    column = new TableColumn(table, SWT.NONE);
 	    column.setText("RealP&L");
 	    column.setWidth(60);
 	
@@ -78,30 +106,70 @@ public class StrategyStatusView extends ViewPart {
 		execComp.setLayout(new GridLayout());
 		execItem.setControl(execComp);
 	
-	    executionTable = new Table(execComp, SWT.MULTI | SWT.FULL_SELECTION);
-	    executionTable.setHeaderVisible(true);
-	    executionTable.setLinesVisible(true);
-	    executionTable.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+	    executionTable = new TableViewer(execComp, SWT.MULTI | SWT.FULL_SELECTION);
+	    executionTable.getTable().setHeaderVisible(true);
+	    executionTable.getTable().setLinesVisible(true);
+	    executionTable.getTable().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+	    executionTable.setContentProvider(new ExecutionContentProvider());
+	    executionTable.setLabelProvider(new ExecutionLabelProvider());
 	
-	    column = new TableColumn(executionTable, SWT.NONE);
+	    table = executionTable.getTable();
+	    column = new TableColumn(table, SWT.NONE);
 	    column.setText("Symb");
 	    column.setWidth(60);
 	    
-	    column = new TableColumn(executionTable, SWT.NONE);
+	    column = new TableColumn(table, SWT.NONE);
 	    column.setText("Dir");
 	    column.setWidth(60);
 	    
-	    column = new TableColumn(executionTable, SWT.NONE);
+	    column = new TableColumn(table, SWT.NONE);
 	    column.setText("Size");
 	    column.setWidth(60);
 	    
-	    column = new TableColumn(executionTable, SWT.NONE);
+	    column = new TableColumn(table, SWT.NONE);
 	    column.setText("Cost");
 	    column.setWidth(60);
 	    
-	    column = new TableColumn(executionTable, SWT.NONE);
+	    column = new TableColumn(table, SWT.NONE);
 	    column.setText("Time");
 	    column.setWidth(60);
+	    
+	    initPositionListener();
+	}
+	
+	private void initPositionListener() {
+		PositionManager.getInstance().addPropertyChangeListener(new PropertyChangeListener() {
+			public void propertyChange(PropertyChangeEvent evt) {
+				
+				if( PROP_RESET.equals(evt.getPropertyName()) ) {
+					positions = new ArrayList<Position>();
+					executions = new ArrayList<JExecution>();
+					positionTable.setInput(positions);
+					executionTable.setInput(executions);
+				} else if( PROP_ADD_POSITION.equals(evt.getPropertyName()) ) {
+					final Position pos = (Position)evt.getNewValue();
+					positions.add(pos);
+					Display.getDefault().asyncExec(new Runnable() {
+						public void run() {
+							positionTable.add(pos);
+							positionTable.refresh(pos);
+						}
+					});
+				} else if( PROP_EXECUTION.equals(evt.getPropertyName()) ) {
+					final Position pos = (Position)evt.getOldValue();
+					final JExecution exec = (JExecution)evt.getNewValue();
+					executions.add(exec);
+					Display.getDefault().asyncExec(new Runnable() {
+						public void run() {
+							positionTable.refresh(pos);
+							executionTable.add(exec);
+							executionTable.refresh(exec);
+						}
+					});
+				}
+			}
+		});
+
 	}
 
 	@Override
@@ -110,4 +178,85 @@ public class StrategyStatusView extends ViewPart {
 		
 	}
 
+	
+	class PositionLabelProvider implements ITableLabelProvider {
+		public Image getColumnImage(Object element, int columnIndex) {
+			return null;
+		}
+		public String getColumnText(Object element, int columnIndex) {
+			Position pos = (Position)element;
+			switch(columnIndex) {
+			case 0:
+				return pos.getInstrument().getSymbol();
+			case 1:
+				return pos.getSide().toString();
+			case 2:
+				return Integer.toString(pos.getQuantity());
+			case 3:
+				return Utils.currencyForm.format(pos.getAvgPrice());
+			case 4:
+				return "unreal";
+			case 5:
+				return "real";	
+			}
+			return "";
+		}
+		public void addListener(ILabelProviderListener listener) {
+		}
+		public void dispose() {
+		}
+		public boolean isLabelProperty(Object element, String property) {
+			return false;
+		}
+		public void removeListener(ILabelProviderListener listener) {
+		}
+	}
+	class PositionContentProvider implements IStructuredContentProvider {
+		public Object[] getElements(Object inputElement) {
+			return ((List)inputElement).toArray();
+		}
+		public void dispose() {
+		}
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		}
+	}
+	class ExecutionLabelProvider implements ITableLabelProvider {
+		public Image getColumnImage(Object element, int columnIndex) {
+			return null;
+		}
+		public String getColumnText(Object element, int columnIndex) {
+			JExecution exec = (JExecution)element;
+			switch(columnIndex) {
+			case 0:
+				return exec.getInstrument().getSymbol();
+			case 1:
+				return exec.getSide().toString();
+			case 2:
+				return Integer.toString(exec.getQuantity());
+			case 3:
+				return Utils.currencyForm.format(exec.getPrice());
+			case 4:
+				return Utils.timeAndDateFormat.format(exec.getDateTime());
+			}
+			return "";
+		}
+		public void addListener(ILabelProviderListener listener) {
+		}
+		public void dispose() {
+		}
+		public boolean isLabelProperty(Object element, String property) {
+			return false;
+		}
+		public void removeListener(ILabelProviderListener listener) {
+		}
+	}
+	class ExecutionContentProvider implements IStructuredContentProvider {
+		public Object[] getElements(Object inputElement) {
+			return ((List)inputElement).toArray();
+		}
+		public void dispose() {
+		}
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		}
+	}
 }
