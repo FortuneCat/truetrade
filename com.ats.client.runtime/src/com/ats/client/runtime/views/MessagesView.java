e608bd850d2a9976519f9158af9af59c9717e957
tyrotrader <tyrotrader@ef3fde84-0b2f-0410-9c2a-95ac4a8bd949>
2007-05-01 23:12:43 +0000

    defect 23: add Log4J messages to MessageView

--- com.ats.client.runtime/src/com/ats/client/runtime/views/MessagesView.java
+++ com.ats.client.runtime/src/com/ats/client/runtime/views/MessagesView.java
@@ -1,14 +1,24 @@
 package com.ats.client.runtime.views;
 
+import java.util.ArrayList;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggingEvent;
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.jface.viewers.IColorProvider;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.List;
 import org.eclipse.ui.IViewSite;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.ViewPart;
 
-import com.ats.db.PlatformDAO;
 import com.ats.engine.IBHelper;
 import com.ats.engine.IBWrapperAdapter;
 import com.ats.platform.MessageListener;
@@ -17,7 +27,8 @@ public class MessagesView  extends ViewPart {
 
 	public static final String ID = "com.ats.client.runtime.views.messagesView";
 
-	private List messageList;
+	private ListViewer messageList;
+	private java.util.List<String> messages = new ArrayList<String>();
 	
 	@Override
 	public void init(IViewSite site) throws PartInitException {
@@ -30,23 +41,51 @@ public class MessagesView  extends ViewPart {
 			public void updateNewsBulletin(int msgId, int msgType, String message, String origExchange) {
 				addMessage("News: from " + origExchange + " [" + message + "]" );
 			}
-			private void addMessage(final String message) {
-				if( messageList != null && ! messageList.isDisposed()) {
-					Display.getDefault().asyncExec(new Runnable() {
-						public void run() {
-							messageList.add(message);
-							//messageList.redraw();
-						}
-					});
-				}
-			}
 		});
 		
+	}
+	private void addMessage(final String message) {
+		if( messageList != null && ! messageList.getList().isDisposed()) {
+			Display.getDefault().asyncExec(new Runnable() {
+				public void run() {
+					messageList.add(message);
+					messageList.reveal(message);
+				}
+			});
+		}
 	}
 
 	@Override
 	public void createPartControl(Composite parent) {
-		messageList = new List(parent, SWT.READ_ONLY | SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER);
+		
+		messageList = new ListViewer(parent, SWT.READ_ONLY | SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER);
+		messageList.setContentProvider(new IStructuredContentProvider() {
+			public Object[] getElements(Object inputElement) {
+				return ((java.util.List)inputElement).toArray();
+			}
+			public void dispose() {
+			}
+			public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+			}
+		});
+		messageList.setLabelProvider(new MessageProvider());
+		
+		Logger.getRootLogger().addAppender(new AppenderSkeleton() {
+			protected void append(final LoggingEvent evt) {
+				if( getLayout() != null ) {
+					addMessage(getLayout().format(evt));
+				} else {
+					String category = evt.getLoggerName();
+					category = category.substring(category.lastIndexOf('.'));
+					addMessage("[" + evt.getLevel() + "][" + category + "]" + evt.getMessage());
+				}
+			}
+			public void close() {
+			}
+			public boolean requiresLayout() {
+				return false;
+			}
+		});
 		
 		// see if this generates some messages
 		IBHelper.getInstance().requestExecutions();
@@ -56,6 +95,19 @@ public class MessagesView  extends ViewPart {
 	public void setFocus() {
 		// TODO Auto-generated method stub
 		
+	}
+
+	class MessageProvider extends LabelProvider implements IColorProvider {
+		public String getText(Object element) {
+			return element.toString();
+		}
+		// TODO: color only works with a table viewer or the fat TextViewer
+		public Color getBackground(Object element) {
+			return ColorConstants.lightBlue;
+		}
+		public Color getForeground(Object element) {
+			return ColorConstants.darkBlue;
+		}
 	}
 
 }
