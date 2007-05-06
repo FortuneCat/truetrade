fdf18bf5f8281ddf415cf6a040104f2b5edd7677
tyrotrader <tyrotrader@ef3fde84-0b2f-0410-9c2a-95ac4a8bd949>
2007-05-06 17:11:09 +0000

    extracted Bar enums to separate files

---
+++ trunk/com.ats.model/src/com/ats/platform/BarField.java
@@ -0,0 +1,10 @@
+package com.ats.platform;
+
+public enum BarField {
+	open,
+	high,
+	low,
+	close,
+	volume
+}
+
---
+++ trunk/com.ats.model/src/com/ats/platform/BarType.java
@@ -0,0 +1,7 @@
+package com.ats.platform;
+
+public enum BarType {
+	time,
+	tick,
+	volume
+}
